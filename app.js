5dde27024f9857813526e61036d9261f91496ab3
Moustafa-Bendari <moustafa.bendari23@gmail.com>
2024-06-14 11:24:54 -0600

    hamster

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 Vue.createApp({
     data() {
       return {
-
+        preloader:true,
       };
     },
     methods: {
         
     },
+    mounted(){
+        setTimeout(() => {
+            this.preloader = false;
+        },2000);
+    }
+    ,
     created: function() {
         console.log("app created")
     }
