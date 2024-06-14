5ae615ca84d79fe0c87f8065d49cd4b90a0f0942
Moustafa-Bendari <moustafa.bendari23@gmail.com>
2024-06-14 10:04:25 -0600

    v1 basics

---
+++ app.js
@@ -0,0 +1,13 @@
+Vue.createApp({
+    data() {
+      return {
+
+      };
+    },
+    methods: {
+        
+    },
+    created: function() {
+        console.log("app created")
+    }
+  }).mount("#app");
